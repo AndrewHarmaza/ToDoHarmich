262b8678e03bec93d680d4c560a68228a52c9838
AndrewHarmaza <303euro@gmail.com>
2021-02-26 19:33:49 +0300

    git push:

--- ToDo.py
+++ ToDo.py
@@ -1,14 +1,50 @@
+print ('''Hi! It's my ToDo''')
 todos = []
-menu = ''
+first = input('Enter first case:  ')
+todos.append (first)
+menu =  '1. Add \n' \
+        '2. Change \n' \
+        '3. Delete \n' \
+        '4. Print \n' \
+        '0. Exit'
 
-def print_todo ():
-    pass
 
-def add_todo (text):
-    pass
+def add_todo():
+    add = input ("")
+    todos.append (add)
 
-def change_todo (index, text):
-    pass
+def change_todo():
+    change_num= int (input('Enter change number: '))
+    change_txt = input ('Enter new case: ')
+    todos [change_num - 1] = change_txt
+    
+def delete_todo():
+	del_num = int (input('Enter del number: '))
+	todos.pop(del_num - 1)
+	
+def print_todos():
+	print(*todos, sep ='\n')
+	
+	
+while True:
+    print (menu)
+    command = input ('')
+    if command not in ['1', '2', '3', '4', '0']:
+        print ('Error')
+        continue
+    if command == '1':
+        add_todo ()
+        continue
+    elif command == '2':
+        change_todo()
+        continue
+    elif command == '3':
+        delete_todo()
+        continue
+    elif command == '4':
+        print_todos()
+        continue
+    elif command == '0':
+        exit
+        break
 
-def delete_todo (index):
-    pass
